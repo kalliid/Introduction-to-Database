16f334f8f250f81d1e7863cee0401cdfe4d94b88
randomp <randomzpq@gmail.com>
2013-02-10 22:24:37 -0800

    file updated

--- SQL/SQL Movie-Rating Query Exercises.sql
+++ SQL/SQL Movie-Rating Query Exercises.sql
@@ -92,3 +92,18 @@ order by avg(stars) DESC, title;
 select name
 from Reviewer
 where Reviewer.rID in (select Rating.rID from Rating group by Rating.rID having count(Rating.rID) > 2);
+
+-- Q1: Add the reviewer Roger Ebert to your database, with an rID of 209.
+insert into Reviewer
+values(209, 'Roger Ebert');
+
+-- Q2: Insert 5-star ratings by James Cameron for all movies in the database. Leave the review date as NULL.
+insert into Rating
+select Reviewer.rID, Movie.mID, 5, null from Reviewer, Movie
+where Reviewer.name = 'James Cameron';
+
+-- Q3: For all movies that have an average rating of 4 stars or higher, add 25 to the release year. (Update the existing tuples; don't insert new tuples.)
+update Movie set year=year+25
+where Movie.mID in (select Rating.mID from Rating
+		                   group by Rating.mID
+	                       having avg(stars)>=4);
